1b507bcba46d938e58a793b4e40983bec0cd3170
b-tudor <btudor@mail.usf.edu>
2022-05-31 18:05:14 -0400

    Trivial update to facilitate remote work. feature incomplete.

--- xyz2lwo/OBJ_group.cpp
+++ xyz2lwo/OBJ_group.cpp
@@ -5,6 +5,7 @@
 
 void OBJ_group::add_points( std::vector<Vector3D> points ) {
 
+	// transfer the passed-in points into the .OBJ's data
 	for( auto p:points )
 		vertices.push_back(p);
 }
@@ -18,6 +19,8 @@ void OBJ_group::add_faces(std::vector<IFF::Face> faces) {
 }
 void OBJ_group::add_faces(std::vector<IFF::Face> polygons, std::string surfaceID) {
 
+	// Transfer the passed-in points into the .OBJ's data. Each polygon references the
+	// points in "vertices" by number, where the numbering of the vertices starts at 1
 	set_material(surfaceID);
 	for (auto p : polygons)
 		faces.push_back(p);
